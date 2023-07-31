0828f411fa3422d879977247048e6d89dc03d8a2
AhmedTayseer50 <120331914+AhmedTayseer50@users.noreply.github.com>
2023-07-31 19:03:13 +0300

    Add files via upload

--- src/component/Section4/Section4.jsx
+++ src/component/Section4/Section4.jsx
@@ -1,12 +1,86 @@
-import React from 'react'
-import './Section4.css'
+import React, { useState } from 'react';
+import './Section4.css';
+
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+} from 'reactstrap';
+
+const items = [
+    {
+        src: '/images/add1.png',
+
+        key: 1,
+    },
+    {
+        src: '/images/add2.png',
+
+        key: 2,
+    },
+];
 
 const Section4 = () => {
-    return (
-        <div className=' main_200'>
-           
-        </div>
-    )
-}
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
 
-export default Section4
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    };
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    };
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    };
+
+    const slides = items.map((item) => {
+        return (
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}
+            >
+                <img src={item.src}  className='images11' />
+
+            </CarouselItem>
+        );
+    });
+
+    return (
+        <Carousel
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+        >
+            <CarouselIndicators
+                items={items}
+                activeIndex={activeIndex}
+                onClickHandler={goToIndex}
+            />
+            {slides}
+            <CarouselControl
+                direction="prev"
+                directionText="Previous"
+                onClickHandler={previous}
+            />
+            <CarouselControl
+                direction="next"
+                directionText="Next"
+                onClickHandler={next}
+            />
+        </Carousel>
+    );
+};
+
+export default Section4;
+
+
